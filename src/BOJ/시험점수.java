c141ab6b4ce6a1551859cade3d5ccd6f88cd7d84
junho <sseq007@naver.com>
2023-07-04 12:58:09 +0900

    day0704

---
+++ src/BOJ/시험점수.java
@@ -0,0 +1,22 @@
+package BOJ;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 시험점수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int total1=0;
+        int total2=0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i <4 ; i++) {
+            total1 += Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i <4 ; i++) {
+            total2 += Integer.parseInt(st.nextToken());
+        }
+        System.out.println(Math.max(total1,total2));
+    }
+}
