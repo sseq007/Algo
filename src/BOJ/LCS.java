0bd21771b668574b33bba01d69a94a1d134ede18
junho <sseq007@naver.com>
2023-11-03 10:42:53 +0900

    day1103

---
+++ src/BOJ/LCS.java
@@ -0,0 +1,27 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LCS {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input1 = br.readLine();
+        String input2 = br.readLine();
+        int[][] dp = new int[input1.length() + 1][input2.length() + 1];
+
+        for (int i = 1; i <=input1.length() ; i++) {
+            for (int j = 1; j <= input2.length(); j++) {
+                if (input1.charAt(i - 1) == input2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j],dp[i][j-1]);
+                }
+            }
+        }
+        System.out.println(dp[input1.length()][input2.length()]);
+
+    }
+}
