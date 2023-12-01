e97ff1d6b9007756cd17448d8fc07e5496abe689
sseq007 <sseq007@naver.com>
2023-12-01 18:15:52 +0900

    day 1201

---
+++ src/BOJ/비밀번호찾기.java
@@ -0,0 +1,29 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class 비밀번호찾기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            map.put(input[0], input[1]);
+        }
+        for (int i = 0; i < m; i++) {
+            String input = br.readLine();
+            System.out.println(map.get(input));
+        }
+    }
+}
--- src/test.java
+++ src/test.java
@@ -2,21 +2,43 @@ import java.util.*;
 
 public class test {
 
+    static class point{
+        int x,y;
+
+        public point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "point{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    '}';
+        }
+    }
     public static void main(String[] args) {
         int[] arr ={1,2,3,1,1,1};
         Map<Integer, Integer> map = new HashMap<>();
 
         for(int a:arr){
             map.put(a,map.getOrDefault(a,0)+1);
-
         }
         List<Integer> list = new ArrayList<>();
         for(int val: map.values()){
             list.add(val);
         }
-        Collections.sort(list,((o1,o2)->o1-o2));
+        Collections.sort(list,((o1,o2)->o2-o1));
         System.out.println(list);
         System.out.println(map.get(1));
 
+        List<point> list2 = new ArrayList<>();
+        list2.add(new point(1, 12));
+        list2.add(new point(3, 11));
+        list2.add(new point(2 ,14));
+        Collections.sort(list2, ((p1, p2) -> p2.y - p1.y));
+
+        System.out.println(list2);
     }
 }
