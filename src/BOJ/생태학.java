fc4aaf75f1ceadda46a5e1a1b24cbe3c4d0455f8
sseq007 <sseq007@naver.com>
2023-12-16 21:00:02 +0900

    day 1216

---
+++ src/BOJ/생태학.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class 생태학 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        int total = 0;
+        Map<String, Integer> map = new HashMap<>();
+        while ((s = br.readLine()) != null) {
+            total++;
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String tree = entry.getKey();
+            list.add(tree);
+        }
+        Collections.sort(list);
+        StringBuilder sb = new StringBuilder();
+        for (String tree : list) {
+            int count = map.get(tree) * 100;
+            sb.append(tree + " " + String.format("%.4f", (double)count / (double)total) + "\n");
+        }
+        System.out.print(sb);
+    }
+}
