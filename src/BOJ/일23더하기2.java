e8b90f770e5842ef1eeb2853bae2c4066ca2a86e
sseq007 <sseq007@naver.com>
2024-01-19 09:40:48 +0900

    day 0119

---
+++ src/BOJ/일23더하기2.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 일23더하기2 {
+
+    static int[] arr;
+    static boolean[] v;
+    static int n,m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = new boolean[n];
+
+    }
+}
