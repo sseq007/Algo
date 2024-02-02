102dcbc15f399e750da8d03ac2dbcba39a1690f4
sseq007 <sseq007@naver.com>
2024-02-03 06:31:03 +0900

    day 0203

---
+++ src/BOJ/꿀따기.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 꿀따기 {
+
+    static int n;
+    static int[] arr;
+    static long Count;
+
+    static long total;
+    static long[] toRightTotal;
+    static long[] toLeftTotal;
+
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
