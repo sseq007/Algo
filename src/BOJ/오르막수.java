13237f39e16d8d99f610b5d6e867d53315c5dc45
sseq007 <sseq007@naver.com>
2024-02-27 09:56:19 +0900

    day 0227

---
+++ src/BOJ/오르막수.java
@@ -0,0 +1,28 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 오르막수 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] dp = new int[n + 1][10];
+        for (int i = 0; i < 10; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = j; k < 10; k++) {
+                    dp[i][j] += dp[i - 1][k];
+                }
+                dp[i][j] %= 10007;
+            }
+        }
+//        System.out.println(Arrays.deepToString(dp));
+        System.out.println(dp[n][0]%10007);
+    }
+}
