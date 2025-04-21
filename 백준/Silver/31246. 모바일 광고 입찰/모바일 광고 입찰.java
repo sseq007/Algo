08455b9d8157fa9da17dad804a40984217c45403
shinjunho <63395794+sseq007@users.noreply.github.com>
2025-04-21 13:59:09 +0900

    [Silver V] Title: 모바일 광고 입찰, Time: 424 ms, Memory: 45068 KB -BaekjoonHub

---
+++ 백준/Silver/31246. 모바일 광고 입찰/모바일 광고 입찰.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[i]=b-a;
+        };
+
+        Arrays.sort(arr);
+
+        System.out.println(Math.max(0,arr[k-1]));
+    }
+}
